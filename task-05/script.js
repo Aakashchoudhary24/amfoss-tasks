442f03fb2281e9682b1b4fd6ebcd8e7d8efff492
Aakash <aakashchoudhary724@gmail.com>
2024-09-03 22:35:23 +0530

    Add functions, update css

--- task-05/script.js
+++ task-05/script.js
@@ -2,7 +2,7 @@ const terminalOutput = document.getElementById('terminal-out');
 const terminalInput = document.querySelector('input[type="text"]');
 const terminal = document.querySelector('.terminal');
 const terminalWindow = document.getElementById('terminal-window');
-
+const cart = [];
 // rendering product data
 let product = null;
 fetch('https://fakestoreapi.com/products/')
@@ -69,25 +69,74 @@ function insertTerminalWindow() {
 }
 // handling the input commands
 function handleInput(command, terminalOutput) {
-    const [cmd, ...arg] = command.split(' ');
+    const [cmd, arg] = command.split(' ');
 
     switch (cmd) {
+
         case 'help':
             helpCommand();
             break;
+
         case 'clear':
-            clear();
+            clearTerminal();
             break;
+
         case 'list':
-            listCommand(terminalOutput);
+            listProducts(terminalOutput);
             break;
+
         case 'details':
             if (arg) {
-                detailsCommand(terminalOutput, arg);
+                viewProductDetails(terminalOutput, arg);
             } else {
-                terminalOutput.innerHTML += "Please provide a product ID.\n";
+                terminalOutput.textContent += "Please provide a product ID.\n";
             }
             break;
+
+        case 'cart':
+            viewCartContent();
+            break;
+
+        case 'add':
+            if (arg) {
+                addToCart(terminalOutput, arg);
+            }
+            else {
+                terminalOutput.textContent += "Please provide a product ID.\n"
+            }
+            break;
+
+        case 'remove':
+            if (arg) {
+                removeFromCart(terminalOutput, arg);
+            }
+            else {
+                terminalOutput.textContent += "Please provide a product ID.\n"
+            }
+            break;
+
+        case 'search':
+            if (arg) {
+                searchCommand(terminalOutput, arg);
+            }
+            else {
+                terminalOutput.textContent += "Please provide a product ID.\n"
+            }
+            break;
+
+        case 'sort':
+            if (arg) {
+                sortCommand(terminalOutput, arg);
+            }
+            else {
+                terminalOutput.textContent += "Please provide a product ID.\n"
+            }
+            break;
+
+        case 'buy':
+            buyCommand();
+            break;
+
         default:
             terminalOutput.textContent += `Invalid command: ${command}\n`;
             break;
@@ -96,8 +145,8 @@ function handleInput(command, terminalOutput) {
 }
 // help command
 function helpCommand(){
-    terminalOutput.innerHTML= 
-    `<p>1) list
+    terminalOutput.textContent = 
+    `1) list
 2) clear
 3) details id
 4) cart
@@ -108,7 +157,7 @@ function helpCommand(){
 9) search "product_name"`
 }
 // clear command
-function clear(){
+function clearTerminal(){
     terminal.innerHTML = '';
 
     const newTerminalWindow = document.createElement('div');
@@ -148,13 +197,13 @@ function clear(){
     terminalWindow.innerHTML = '';
 }
 // list command 
-function listCommand(terminalOutput) {
+function listProducts(terminalOutput) {
     product.forEach(p => {
         terminalOutput.innerHTML += `${p.id} : ${p.title}<br>`;
     });
 }
 // view product details command
-function detailsCommand(terminalOutput, productId){
+function viewProductDetails(terminalOutput, productId){
     terminalOutput.innerHTML = '';
     const productDetail = product.find(p => p.id === parseInt(productId));
     terminalOutput.innerHTML += 
@@ -162,4 +211,50 @@ function detailsCommand(terminalOutput, productId){
                 <p>Price : $${productDetail.price}</p>
                 <p>Description : ${productDetail.description}</p>
                 <p>Category : ${productDetail.category}</p>`;
+}
+// essential function
+function locateProductInCart(cart, value){
+    const totalItems = cart.length;
+    for(i = 0; i < totalItems; i++){
+        if (cart[i] == value){
+            return i;
+        }
+        else {
+            return false;
+        }
+    }
+}
+// add to cart
+function addToCart(terminalOutput, productId){
+
+    const productToAdd = product.find(p => p.id === parseInt(productId));
+    const thisId = productToAdd.id
+    if(!productToAdd){
+        terminalOutput.textContent += "No product with provided ID found\n"
+    }
+    else if (locateProductInCart(cart, thisId)){
+        terminalOutput.textContent += "Item already in cart"
+    }
+    else{
+        cart.push(thisId);
+        terminalOutput.innerHTML += 
+        `Product ${thisId} successfully added to cart . Type 'cart' to view cart details`;
+    }
+}
+// remove from cart
+function removeFromCart(terminalOutput, productId){
+
+    productIndexInCart = locateProductInCart(cart, productId);
+
+    const individualProduct = product.find(p => p.id === parseInt(productId));
+    if (productId > 20){
+        terminalOutput.textContent += "No product with provided ID\n"
+    }
+    else if (!individualProduct){
+        terminalOutput.textContent += "Product not in cart... yet :)"
+    }
+    else{
+        cart.pop(productIndexInCart);
+        terminalOutput.textContent += `Product ${productId} removed successfully`;
+    }
 }
